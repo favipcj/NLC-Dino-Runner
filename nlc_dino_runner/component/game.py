4d5104688a67695374ba413c5283bfce2c8f8ba3
carito30o <93555232+carito30o@users.noreply.github.com>
2021-11-05 23:18:37 -0400

    Class3:
    en esta clase nos encargamos de implementar el dinosaurio con todos sus atributos de saltar, correr y agacharse, en esta clase usamos conocimientos de el modulo anterior.
    
    fuimos armando el codigo para luego complemerta el codigo con las imagenes respectivas para cada funcion como ser correr, agacharse, saltar asi tambien modificando algunas cosas del codigo de la clase anterior  para que se añada nuestro dinosaurio con sus funciones respectivas.

--- nlc_dino_runner/component/game.py
+++ nlc_dino_runner/component/game.py
@@ -1,24 +1,25 @@
 import pygame
 
 from nlc_dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITTLE, FPS
+from nlc_dino_runner.component.dinosaur import Dinosaur
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.playing = False
         pygame.display.set_caption(TITTLE)
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
+        self.playing = False
         self.game_speed = 20
         self.x_pos_bg = 0
-        self.y_pos_bg = 400
+        self.y_pos_bg = 380
+        self.player=Dinosaur()
 
     def run(self):
-
         self.playing = True
-        while self. playing:
+        while self.playing:
             self.events()
             self.update()
             self.draw()
@@ -27,22 +28,25 @@ class Game:
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.playing = False
+                self.playing=False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
-        self.screen.fill((255, 255, 255))
+        self.screen.fill((255,255,255))
         self.draw_background()
+        self.player.draw(self.screen)
+        pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
-        image_width = BG.get_width()
-        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
-        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+        image_width=BG.get_width()
+        self.screen.blit(BG,(self.x_pos_bg,self.y_pos_bg))
+        self.screen.blit(BG,(image_width+self.x_pos_bg,self.y_pos_bg))
         if self.x_pos_bg <= -image_width:
-            self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
-            self.x_pos_bg = 0
-        self.x_pos_bg -= self.game_speed
+            self.screen.blit(BG,(image_width+self.x_pos_bg,self.y_pos_bg))
+            self.x_pos_bg=0
+        self.x_pos_bg-=self.game_speed
